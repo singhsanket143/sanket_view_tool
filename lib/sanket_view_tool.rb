5373be6b8610a8af5d0d0a7c827ddbb46b7aa710
Sanket Singh <singhsanket143@gmail.com>
2018-05-23 02:40:42 +0530

    Gem initial commit

---
+++ lib/sanket_view_tool.rb
@@ -0,0 +1,5 @@
+require "sanket_view_tool/version"
+require "sanket_view_tool/renderer"
+module SanketViewTool
+  # Your code goes here...
+end
---
+++ lib/sanket_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SanketViewTool
+  VERSION = "0.1.0"
+end
